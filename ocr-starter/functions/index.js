384d880087c4655f191a332445ab381f9effda70
Rune Niska <niska.rune@gmail.com>
2022-12-20 14:35:42 +0200

    Add receipts from cloud functions

--- ocr-starter/functions/index.js
+++ ocr-starter/functions/index.js
@@ -2,6 +2,8 @@ import functions, { logger } from 'firebase-functions';
 import vision from '@google-cloud/vision';
 import admin from 'firebase-admin';
 
+admin.initializeApp();
+
 export const readReceiptDetails = functions.storage.object().onFinalize(async (object) => {
   const imageBucket = `gs://${object.bucket}/${object.name}`;
   const client = new vision.ImageAnnotatorClient();
@@ -10,4 +12,25 @@ export const readReceiptDetails = functions.storage.object().onFinalize(async (o
   const [annotation] = textDetections.textAnnotations;
   const text = annotation ? annotation.description : '';
   logger.log(text);
+
+  // @TODO Parse text
+
+  // Get user ID
+  // object.name is userID/timestamp
+  const re = /(.*)\//;
+  const uid = re.exec(object.name)[1];
+
+  // Going to hardcode returned text for relevant fields
+  const receipt = {
+    address: 'Jätkäsaari',
+    amount: '210.97',
+    date: new Date(),
+    imageBucket,
+    items: 'Air Fryer, Poppamies Majoneesi, Weekender Bag',
+    locationName: 'Verkkokauppa.com',
+    isConfirmed: false,
+    uid
+  };
+
+  admin.firestore().collection('receipts').add(receipt);
 });
